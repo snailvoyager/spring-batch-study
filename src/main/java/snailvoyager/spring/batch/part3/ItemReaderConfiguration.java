4b31f1d8a9fd6cc85ed1adcff50fc13f075be91f
snailvoyager <kyj88514@gmail.com>
2023-01-15 22:27:55 +0900

    CustomItemReader 구현

---
+++ src/main/java/snailvoyager/spring/batch/part3/ItemReaderConfiguration.java
@@ -0,0 +1,55 @@
+package snailvoyager.spring.batch.part3;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class ItemReaderConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job itemReaderJob() {
+        return jobBuilderFactory.get("itemReaderJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.customItemReaderStep())
+                .build();
+    }
+
+    @Bean
+    public Step customItemReaderStep() {
+        return stepBuilderFactory.get("customItemReaderStep")
+                .<Person, Person>chunk(10)
+                .reader(new CustomItemReader<>(getItems())) //ItemStream은 ExecutionContext로 read, write 정보를 저장
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemWriter<Person> itemWriter() {
+        return items -> log.info(items.stream()
+                .map(Person::getName)
+                .collect(Collectors.joining(", ")));
+    }
+
+    private List<Person> getItems() {
+        List<Person> items = new ArrayList<>();
+        for (int i=0; i<10; i++) {
+            items.add(new Person(i+1, "test name" + i, "test age", "test address"));
+        }
+        return items;
+    }
+}
