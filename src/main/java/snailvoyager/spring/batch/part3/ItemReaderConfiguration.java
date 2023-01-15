1a66582f45bc8157285769d777846c3f39602a25
snailvoyager <kyj88514@gmail.com>
2023-01-15 23:30:17 +0900

    FlatFileItemReader 구현

--- src/main/java/snailvoyager/spring/batch/part3/ItemReaderConfiguration.java
+++ src/main/java/snailvoyager/spring/batch/part3/ItemReaderConfiguration.java
@@ -8,8 +8,13 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,10 +28,11 @@ public class ItemReaderConfiguration {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job itemReaderJob() {
+    public Job itemReaderJob() throws Exception {
         return jobBuilderFactory.get("itemReaderJob")
                 .incrementer(new RunIdIncrementer())
                 .start(this.customItemReaderStep())
+                .next(this.csvFileStep())
                 .build();
     }
 
@@ -37,6 +43,40 @@ public class ItemReaderConfiguration {
                 .reader(new CustomItemReader<>(getItems())) //ItemStream은 ExecutionContext로 read, write 정보를 저장
                 .writer(itemWriter())
                 .build();
+    }
+
+    @Bean
+    public Step csvFileStep() throws Exception {
+        return stepBuilderFactory.get("csvFileStep")
+                .<Person, Person>chunk(10)
+                .reader(csvFileItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+    private FlatFileItemReader<Person> csvFileItemReader() throws Exception {
+        DefaultLineMapper<Person> lineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+        tokenizer.setNames("id", "name", "age","address");
+        lineMapper.setLineTokenizer(tokenizer);
+
+        lineMapper.setFieldSetMapper(fieldSet -> {
+            int id = fieldSet.readInt("id");
+            String name = fieldSet.readString("name");
+            String age = fieldSet.readString("age");
+            String address = fieldSet.readString("address");
+            return new Person(id, name, age, address);
+        });
+
+        FlatFileItemReader<Person> itemReader = new FlatFileItemReaderBuilder<Person>()
+                .name("csvFileItemReader")
+                .encoding("UTF-8")
+                .resource(new ClassPathResource("test.csv"))    //resources 디렉토리 아래 파일 읽기
+                .linesToSkip(1)     //두번째 라인부터 읽기
+                .lineMapper(lineMapper)
+                .build();
+        itemReader.afterPropertiesSet();       //itemReader 검증
+
+        return itemReader;
     }
 
     private ItemWriter<Person> itemWriter() {
