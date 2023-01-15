942dcbbe8f664e00fc4565b008457ded3ae6bffa
snailvoyager <kyj88514@gmail.com>
2023-01-15 20:14:51 +0900

    tasklet, chunk 기반 페이징 처리

---
+++ src/main/java/snailvoyager/spring/batch/part3/ChunkProcessingConfiguration.java
@@ -0,0 +1,98 @@
+package snailvoyager.spring.batch.part3;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class ChunkProcessingConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job chunkProcessingJob() {
+        return jobBuilderFactory.get("chunkProcessingJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.taskBaseStep())
+                .next(this.chunkBaseStep())
+                .build();
+    }
+
+    @Bean
+    public Step chunkBaseStep() {
+        return stepBuilderFactory.get("chunkBaseStep")
+                .<String, String>chunk(10)  //데이터를 10개씩 나눔. 1 Generic : reader 반환타입, 2 Generic : processor 반환타입
+                .reader(itemReader())                //null를 리턴할 때까지 반복
+                .processor(itemProcessor())          //
+                .writer(itemWriter())                //processor에서 받은 stream을 모아서 한번에 처리
+                .build();
+    }
+
+    private ItemWriter<String> itemWriter() {
+        return items -> log.info("chunk item size : {}", items.size());
+//        return items -> items.forEach(log::info);
+    }
+
+    private ItemProcessor<String, String> itemProcessor() {
+        return item -> item + ", Spring Batch";
+    }
+
+    private ItemReader<String> itemReader() {
+        return new ListItemReader<>(getItems());
+    }
+
+    @Bean
+    public Step taskBaseStep() {
+        return stepBuilderFactory.get("taskBaseStep")
+                .tasklet(this.tasklet())
+                .build();
+    }
+
+    private Tasklet tasklet() {
+        List<String> items = getItems();
+
+        return (contribution, chunkContext) -> {        //tasklet paging 처리
+            StepExecution stepExecution = contribution.getStepExecution();
+            int chunkSize = 10;
+            int fromIndex = stepExecution.getReadCount();
+            int toIndex = fromIndex + chunkSize;
+
+            if (fromIndex >= items.size()) {
+                return RepeatStatus.FINISHED;
+            }
+
+            List<String> subList = items.subList(fromIndex, toIndex);
+            log.info("task item size : {}", subList.size());
+
+            stepExecution.setReadCount(toIndex);
+
+            return RepeatStatus.CONTINUABLE;
+        };
+    }
+
+    private List<String> getItems() {
+        List<String> items = new ArrayList<>();
+        for (int i=0; i<100; i++) {
+            items.add(i + " Hello");
+        }
+        return items;
+    }
+}
